455d48b72a08e62532351e1768a5790ab520f1ae
Mayank Jajpure <mayankjajpure02@gmail.com>
2023-05-06 23:29:19 +0530

    Add Blog Table

--- DevHelps/DevHelps/settings.py
+++ DevHelps/DevHelps/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home",
+    "django_tiptap",
+    "froala_editor"
+
 ]
 
 MIDDLEWARE = [
--- DevHelps/home/models.py
+++ DevHelps/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from froala_editor.fields import FroalaField
+from .utils import genrateslug
 
 # Create your models here.
+
+class BlogModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content =  FroalaField()
+    slug = models.SlugField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to="blog", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = genrateslug(self.title)
+        super(BlogModel, self).save(*args, **kwargs)
+
+
+
+
+
+
