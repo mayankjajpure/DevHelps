bea2c146b3a3e5b6afdce100216c7f274a7da4ee
Mayank Jajpure <mayankjajpure02@gmail.com>
2023-05-07 19:04:39 +0530

    feat: add view blog and feeds

--- DevHelps/home/urls.py
+++ DevHelps/home/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path("",home, name="landing_page"),
     path("home/",home, name="landing_page"),
     path("about/",about, name="about"),
+    path('contact/',contact),
+    path('feeds/',feeds),
+    path('blog/<slug:slug>/',viewblog, name="viewblog"),
+    
+
+
+
 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
